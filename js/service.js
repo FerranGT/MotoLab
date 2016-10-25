d90e18cedc8477bfca8dbbc777d128763091d311
Judit <Judith@MacBook-Air-de-Judith.local>
2016-10-25 16:19:50 +0200

    inserting bike working

--- js/service.js
+++ js/service.js
@@ -1,6 +1,12 @@
 angular.module('myServices', [])
 	.factory("motodbservice", function($http) {
 
+			var motorbikes = [];
+				var lsMotorbikes = localStorage.getItem("motorbikes");
+				if ( lsMotorbikes ) {
+					motorbikes = JSON.parse(lsMotorbikes)
+				}
+
 			function getMotoBrand() {
 				return $http.get("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/Moto?format=json");
 			}
@@ -9,12 +15,38 @@ angular.module('myServices', [])
 				return $http.get("https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/" + idBrand + "/vehicleType/moto?format=json");
 			}
 
-			function pushMoto() {
-				return $http.get("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/Moto?format=json");
+			function pushMoto(idBrand,idModel,price,year,image,description) {
+
+				var bike = {
+					brand:idBrand,
+					model:idModel,
+					price:price,
+					year:year,
+					url_img:image,
+					description:description,
+				};
+
+				
+				var numBikes = motorbikes.length;
+				var lastIdAvailable ;
+
+				if (numBikes) {
+					lastIdAvailable = motorbikes[numBikes-1].id + 1;
+				}
+				else {
+					lastIdAvailable	= 1;
+				}
+
+				bike.id = lastIdAvailable;
+				motorbikes.push(bike);
+				var jsonBikes = JSON.stringify(motorbikes);
+				localStorage.setItem("motorbikes", jsonBikes);
+				console.log(localStorage.getItem("motorbikes"));
 			}
 
 			return {
 				getMotoBrand: getMotoBrand,
-				getMotoModel: getMotoModel
+				getMotoModel: getMotoModel,
+				pushMoto: pushMoto
 			}
 	})

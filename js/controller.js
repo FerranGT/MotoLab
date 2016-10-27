4214c941d178fa721f00f2002cd9bdfc9b487c99
Daniel Muñoz Ruiz <Daniel@Mac-mini.local>
2016-10-27 14:57:49 +0200

    add content

--- js/controller.js
+++ js/controller.js
@@ -89,7 +89,7 @@ angular.module("myControllers",['myServices'])
 		$rootScope.activetab = 'home';
 		$rootScope.bikes = motodbservice.bikes;
 		$scope.showBikes = function(){
-			window.location = "#/moto"
+			window.top.location = "#/moto"
 		}
 	})
 
